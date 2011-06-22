3458d1c678b986c3fd553ad5d8ac48ab8c95f2f5
Diego Restrepo <rescolo@gmail.com>
2011-06-22 01:27:12 -0500

    Now pyspheno is completelly independent of external files, and EXTPAR is only written when non-universality is used. TODO: make LesHouces dictionary a local variable

--- pyspheno.py
+++ pyspheno.py
@@ -5,11 +5,91 @@ import commands
 import pyslha
 import sys
 
-global sphenocmd,LesHouches,decaysin
-sphenocmd='SPheno_intel'
+#DEBUG: get rid of global variable: LesHouches.
+#Must have the MASS block to be read
+#LesHouches,decaysin=pyslha.readSLHAFile('LesHouches_MASS.in')
 
-#Must hava the MASS block to be read
-LesHouches,decaysin=pyslha.readSLHAFile('LesHouches_MASS.in')
+def buildLHAinFile(universal=True):
+    """Usage of pyslha.Block to generate one LesHouches.in file"""
+    #Initialize dictionary of block clases
+    xdict={}
+    #define block class
+    MODSEL=pyslha.Block('MODSEL') # Select model
+    #Add entries to the class
+    MODSEL.entries[1]=1 # mSUGRA
+    #Add class to dictionary
+    xdict['MODSEL']=MODSEL
+    #====================
+    SMINPUTS=pyslha.Block('SMINPUTS')   # Standard Model inputs
+    SMINPUTS.entries[1]=1.279340E+02       # alpha_rm^-1(M_Z), MSbar, SM
+    SMINPUTS.entries[2]=1.166390E-05       # G_F, Fermi constant
+    SMINPUTS.entries[3]=1.172000E-01       # alpha_s(MZ) SM MSbar
+    SMINPUTS.entries[4]=9.118760E+01       # Z-boson pole mass
+    SMINPUTS.entries[5]=4.250000E+00       # m_b(mb) SM MSbar
+    SMINPUTS.entries[6]=1.727000E+02       # m_top(pole)
+    SMINPUTS.entries[7]=1.777000E+00       # m_tau(pole)
+    xdict['SMINPUTS']=SMINPUTS
+    #====================
+    MINPAR=pyslha.Block('MINPAR') # Input parameters
+    MINPAR.entries[1]=1000                # M0
+    MINPAR.entries[2]=240                # m_1/2
+    MINPAR.entries[3]=1.000000E+01       # tanb
+    MINPAR.entries[4]=1                  # sign(mu)
+    MINPAR.entries[5]=-1.000000E+02       # A0
+    xdict['MINPAR']=MINPAR
+    #====================
+    if not universal:
+        EXTPAR=pyslha.Block('EXTPAR')     # Gaugino masses
+        EXTPAR.entries[1]=240      # M_1  240
+        EXTPAR.entries[2]=240      # M_2  240 134 270
+        EXTPAR.entries[3]=240      # M_3  240
+        xdict['EXTPAR']=EXTPAR
+    #====================
+    RVSNVEVIN=pyslha.Block('RVSNVEVIN')   # sneutrino vevs at Q
+    RVSNVEVIN.entries[1]=-6.12717708E-03  # v_L_1
+    RVSNVEVIN.entries[2]= 6.60031840E-03  # v_L_2
+    RVSNVEVIN.entries[3]=-6.34389921E-03  # v_L_3
+    xdict['RVSNVEVIN']=RVSNVEVIN
+    #====================
+    RVKAPPAIN=pyslha.Block('RVKAPPAIN')  # bilinear RP parameters at Q
+    RVKAPPAIN.entries[1]= 1.60784795E-01  # epsilon_1
+    RVKAPPAIN.entries[2]=-1.67970270E-01  # epsilon_2
+    RVKAPPAIN.entries[3]= 1.71230537E-01  # epsilon_3
+    xdict['RVKAPPAIN']=RVKAPPAIN
+    #====================
+    SPHENOINPUT=pyslha.Block('SPHENOINPUT')  # SPheno specific input
+    SPHENOINPUT.entries[ 1]= 0		    # error level
+    SPHENOINPUT.entries[ 2]= 0		    # if =1, then SPA conventions are used
+    SPHENOINPUT.entries[11]= 1		    # calculate branching ratios
+    SPHENOINPUT.entries[12]= 1.00000000E-06 # write only branching ratios larger than this value
+    SPHENOINPUT.entries[21]= 0		    # calculate cross section 
+    SPHENOINPUT.entries[22]= 5.00000000E+02 # cms energy in GeV
+    SPHENOINPUT.entries[23]= 0.00000000E+00 # polarisation of incoming e- beam
+    SPHENOINPUT.entries[24]= 0.00000000E+00 # polarisation of incoming e+ beam
+    SPHENOINPUT.entries[25]= 0		    # if 0 no ISR is calculated, if 1 ISR is caculated
+    SPHENOINPUT.entries[26]= 1.00000000E-02 # write only cross sections larger than this value [fb]
+    SPHENOINPUT.entries[31]=-1.00000000E+00 # m_GUT, if < 0 than it determined via g_1=g_2
+    SPHENOINPUT.entries[32]= 0              # require strict unification g_1=g_2=g_3 if '1' is set
+    SPHENOINPUT.entries[41]= 2.49520000E+00 # width of the Z-boson
+    SPHENOINPUT.entries[42]= 2.11800000E+00 # width of the W-boson
+    SPHENOINPUT.entries[90]= 1		    # if R-parity is added
+    SPHENOINPUT.entries[91]= 1		    # Neutrino Fit on
+    xdict['SPHENOINPUT']=SPHENOINPUT
+    #====================
+    NEUTRINOBOUNDSIN=pyslha.Block('NEUTRINOBOUNDSIN')  # The values are from 0808.2016 for 3sigma 
+    NEUTRINOBOUNDSIN.entries[1 ]=2.07000000E-21	     # Delta m^2 atm, min
+    NEUTRINOBOUNDSIN.entries[2 ]=2.75000000E-21	      # Delta m^2 atm, max
+    NEUTRINOBOUNDSIN.entries[3 ]=5.62500000E-01	      # tan^2 atm, min
+    NEUTRINOBOUNDSIN.entries[4 ]=2.03000000E+00	      # tan^2 atm, max
+    NEUTRINOBOUNDSIN.entries[5 ]=7.05000000E-23	      # lower bound on solar
+    NEUTRINOBOUNDSIN.entries[6 ]=8.34000000E-23	      # upper bound on solar
+    NEUTRINOBOUNDSIN.entries[7 ]=3.33333300E-01	      # tan^2 sol, min
+    NEUTRINOBOUNDSIN.entries[8 ]=5.87300000E-01	      # tan^2 sol, max
+    NEUTRINOBOUNDSIN.entries[9 ]=0.00000000E+00	      # U^2e3,min
+    NEUTRINOBOUNDSIN.entries[10]= 5.6000000E-02	      # U^2e3,max
+    xdict['NEUTRINOBOUNDSIN']=NEUTRINOBOUNDSIN
+    #====================
+    return xdict 
 
 def changeLHAinFile(x):
     """Change specfic entries of the global dictionary:
@@ -18,42 +98,39 @@ def changeLHAinFile(x):
         LesHouches['RVKAPPAIN'].entries[i]=x[i-1] # epsilon_i
         LesHouches['RVSNVEVIN'].entries[i]=x[i+2] # v_L_i
 
-def FilterDecay(xdict,pid=1000022,nda=3,pdaug=11):
+def writeLHAinFile(xdict,lhinfile='LesHouches.in',universal=True):
+    '''To write LesHouches.in in the right order.'''
+    LesHouches2={}
+    LesHouches2['AMODSEL']=xdict['MODSEL']
+    LesHouches2['BSMINPUTS']=xdict['SMINPUTS']
+    LesHouches2['CMINPAR']=xdict['MINPAR']
+    if not universal: LesHouches2['DEXTPAR']=xdict['EXTPAR']
+    LesHouches2['ERVSNVEVIN']=xdict['RVSNVEVIN']
+    LesHouches2['FRVKAPPAIN']=xdict['RVKAPPAIN']
+    LesHouches2['GSPhenoInput']=xdict['SPHENOINPUT']
+    LesHouches2['HNEUTRINOBOUNDSIN']=xdict['NEUTRINOBOUNDSIN']
+    pyslha.writeSLHAFile(lhinfile,LesHouches2,{})
+
+
+def filterDecay(xdict,pid,pdaug):
     '''Build filtered pyslha decay dictionary from:
          xdict
        original pyslha dictionary, for particle number:
          pid
-       decaying into:
-         nda
-       particles, with:
+       decaying into a set of particles which includes
          pdaug
-       between the daughters
+       between the daughters.
     '''
     prtclnda=pyslha.Particle(pid,xdict[pid].totalwidth,xdict[pid].mass)
     filterdict={}
-    if nda==3:
-        prtclnda.decays=[channel for channel in xdict[pid].decays if channel.nda==nda \
-                        and (abs(channel.ids[0])==pdaug or abs(channel.ids[1])==pdaug \
-                             or abs(channel.ids[2])==pdaug)]
-
-    if nda==2:
-        prtclnda.decays=[channel for channel in xdict[pid].decays if channel.nda==nda \
-                        and (abs(channel.ids[0])==pdaug or abs(channel.ids[1])==pdaug)]
-
+    list1=[channel for channel in xdict[pid].decays if channel.nda==2 \
+                and (abs(channel.ids[0])==pdaug or abs(channel.ids[1])==pdaug)]
+    prtclnda.decays=list1+[channel for channel in xdict[pid].decays \
+                        if channel.nda==3 and \
+                      (abs(channel.ids[0])==pdaug or abs(channel.ids[1])==pdaug \
+                         or abs(channel.ids[2])==pdaug)]
     filterdict[pid]=prtclnda
     return filterdict
-
-def writeLHAinFile(xdict,lhinfile='LesHouches.in',universal=True):
-    '''To write LesHouches.in in the right order.'''
-    if universal:
-        xdict['EXTPAR'].entries={}
-        
-    LesHouches2={'AMODSEL':xdict['MODSEL'],'BSMINPUTS':xdict['SMINPUTS'],\
-                 'CMINPAR':xdict['MINPAR'],'DEXTPAR':xdict['EXTPAR'],\
-                 'ERVSNVEVIN':xdict['RVSNVEVIN'],'FRVKAPPAIN':xdict['RVKAPPAIN'],\
-                 'GSPhenoInput':xdict['SPHENOINPUT'],\
-                 'HNEUTRINOBOUNDSIN':xdict['NEUTRINOBOUNDSIN']}
-    pyslha.writeSLHAFile(lhinfile,LesHouches2,{})
 
 def oscilation(spcfile):
     """oscilation parameters"""
@@ -140,7 +217,9 @@ def optloop(ifin=1,minimum=False,mu=100,vd=100):
     return xmin
     
 if __name__ == '__main__':
-    #Loop over initial x
+    global sphenocmd,LesHouches
+    sphenocmd='SPheno_intel'
+    LesHouches=buildLHAinFile() #see below
     #to modify the dictionary of clases:
     LesHouches['SPHENOINPUT'].entries[91]=0
     minimum=False
@@ -148,7 +227,7 @@ if __name__ == '__main__':
         ifin=1
         mu=100;vd=100 #not used at all
     else:
-        ifin=100000
+        ifin=1#100000
 
     #obtain vd and mu for msugra fixed parameters
     LesHouches['SPHENOINPUT'].entries[91]=1
--- pysphenofull.py
+++ pysphenofull.py
@@ -1,17 +1,31 @@
+#!/usr/bin/env python
 import pyspheno
 import commands
 if __name__ == '__main__':
+    global sphenocmd,LesHouches
+    sphenocmd='SPheno_intel'
+    LesHouches=pyspheno.buildLHAinFile() #see below
+
     #Loop over initial x
     #obtain vd and mu for msugra fixed parameters
     pyspheno.LesHouches['SPHENOINPUT'].entries[91]=1
-    pyspheno.writeLHAinFile(pyspheno.LesHouches,'LesHouches.in')
-    lsout=commands.getoutput(pyspheno.sphenocmd)
-    spc,decays=pyspheno.pyslha.readSLHAFile('SPheno.spc')
-    chietau=pyspheno.FilterDecay(pyspheno.FilterDecay(decays,pdaug=11),pdaug=15)
-    chietaubr=sum([x.br for x in chietau[1000022].decays])
-    chimutau=pyspheno.FilterDecay(pyspheno.FilterDecay(decays,pdaug=13),pdaug=15)
-    chimutaubr=sum([x.br for x in chimutau[1000022].decays])
-    print 'solar mixing %g' %(chietaubr/chimutaubr)
+    #m12min=200;m12max=800
+    #m0min=200;m0max=1000
+    m12min=70;m12max=70
+    m0min=100;m0max=100
+    step=50
+    for m12 in pyspheno.np.arange(m12min,m12max+step,step):
+        for m0 in pyspheno.np.arange(m0min,m0max+step,step):
+            pyspheno.LesHouches['MINPAR'].entries[1]=m0
+            pyspheno.LesHouches['MINPAR'].entries[2]=m12
+            pyspheno.writeLHAinFile(pyspheno.LesHouches,'LesHouches.in')
+            lsout=commands.getoutput(pyspheno.sphenocmd)
+            spc,decays=pyspheno.pyslha.readSLHAFile('SPheno.spc')
+            chietau=pyspheno.filterDecay(pyspheno.filterDecay(decays,1000022,11),1000022,15)
+            chietaubr=sum([x.br for x in chietau[1000022].decays])
+            chimutau=pyspheno.filterDecay(pyspheno.filterDecay(decays,1000022,13),1000022,15)
+            chimutaubr=sum([x.br for x in chimutau[1000022].decays])
+            print 'solar mixing %d %d %g' %(m12,m0,chietaubr/chimutaubr)
 
     
 #Fron RpParaneters.dat in SPhenoRP_intel/test
