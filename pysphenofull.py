5036f57d65b63c55b8efddf5babfe7ee6fa7d37b
Diego Restrepo <rescolo@gmail.com>
2011-06-24 12:55:36 -0500

    Mayor reorganization of files. Now pyspheno.py is general module to manipulate the I/O of SPheno, extending pyslha. Depends only in pyslha, other dependences have been moved to the example files: pysphenorandom.py is an example of use of pyspheno to calculate neutrino masses and mixings, while pysphenofull.py is an example to scan m0 and m1/2 and to analyze the decays in each SPheno.spc

--- pysphenofull.py
+++ pysphenofull.py
@@ -1,40 +1,36 @@
 #!/usr/bin/env python
-import pyspheno
+from pyspheno import * #also imports pyslha
 import commands
-if __name__ == '__main__':
-    global sphenocmd 
-    sphenocmd='SPheno_intel'
-    LesHouches=pyspheno.buildLHAinFile() #see below
 
-    #Loop over initial x
-    #obtain vd and mu for msugra fixed parameters
+if __name__ == '__main__':
+    #Set the SPheno command, e.g: ./SPheno, or:
+    sphenocmd='SPheno_intel'
+    #Build SPhheno input file:
+    LesHouches=buildLHAinFile() 
+    #Be sure that the neutrino fit is switched on:
     LesHouches['SPHENOINPUT'].entries[91]=1
     #m12min=200;m12max=800
     #m0min=200;m0max=1000
     m12min=240;m12max=240
     m0min=1000;m0max=1000
     step=50
-    for m12 in pyspheno.np.arange(m12min,m12max+step,step):
-        for m0 in pyspheno.np.arange(m0min,m0max+step,step):
+    for m12 in range(m12min,m12max+step,step): #must be integers
+        for m0 in range(m0min,m0max+step,step): #must be integers
             LesHouches['MINPAR'].entries[1]=m0
             LesHouches['MINPAR'].entries[2]=m12
-            pyspheno.writeLHAinFile(LesHouches,'LesHouches.in')
+            writeLHAinFile(LesHouches,'LesHouches.in')
             lsout=commands.getoutput(sphenocmd)
-            spc,decays=pyspheno.pyslha.readSLHAFile('SPheno.spc')
-            chietau=pyspheno.filterDecay(pyspheno.filterDecay(decays,1000022,11),1000022,15)
-            chietaubr=sum([x.br for x in chietau[1000022].decays])
-            chimutau=pyspheno.filterDecay(pyspheno.filterDecay(decays,1000022,13),1000022,15)
-            chimutaubr=sum([x.br for x in chimutau[1000022].decays])
+            #decays is a dictionary containing the particle objects with keys
+            #given by PDG particle identificaction numbers (PIN).
+            #Check pyslha doc:
+            spc,decays=pyslha.readSLHAFile('SPheno.spc')
+            #Extract particle object with key PIN=1000022, neutralino, with
+            #only decays channels having particle PIN=11 and PIN=15.
+            chietau=filterParticle(filterParticle(decays[1000022],11),15)
+            chietaubr=sum([x.br for x in chietau.decays])
+            chimutau=filterParticle(filterParticle(decays[1000022],13),15)
+            chimutaubr=sum([x.br for x in chimutau.decays])
             print 'solar mixing %d %d %g' %(m12,m0,chietaubr/chimutaubr)
 
     
-#Fron RpParaneters.dat in SPhenoRP_intel/test
-#3E-02 0.6 ! epsilon_1
-#1.E-05 1. ! epsilon_2
-#1.E-05 1. ! epsilon_3
-#1.E-05 6.E-02 ! Lambda_1 = v_d epsilon_1 + mu v_L1
-#3.E-02 7E-01 ! Lambda_2 = v_d epsilon_2 + mu v_L2
-#3.E-02 7E-01 ! Lambda_3 = v_d epsilon_3 + mu v_L3
-
-
 #
